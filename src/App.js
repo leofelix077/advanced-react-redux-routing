40d9143774361db88a9538a3643ccbca79f9a6c4
Leonardo Felix <leonardoaalf077@hotmail.com>
2018-11-03 04:06:35 -0300

    Boilerplate code

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import { View } from 'react-native';
 import { Provider } from "react-redux"
 import { createStore } from "redux"
 import { Header } from "./components/common"
+import reducers from "./reducers"
+import firebase from "firebase"
 
 class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyCawX6HM7qLKSXB-dJkNz3NUIqOX2bgeJY",
+      authDomain: "react-native-routes.firebaseapp.com",
+      databaseURL: "https://react-native-routes.firebaseio.com",
+      projectId: "react-native-routes",
+      storageBucket: "",
+      messagingSenderId: "599614273503"
+    };
+    firebase.initializeApp(config);
+  }
 
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View style={{ flex: 1 }}>
-          <Header headerText="React-Redux Demo" />
+          <Header headerText="React-Redux Advanced" />
         </View>
       </Provider>
     );
